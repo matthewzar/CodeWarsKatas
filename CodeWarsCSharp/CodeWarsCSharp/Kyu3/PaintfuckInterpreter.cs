77af463416817e774bf28bb2ea9a34767bd9cb9a
Matthew Funcke <matthew.funcke@derivco.co.za>
2017-11-10 15:35:05 +0200

    Added skeleton code from paintfuck kata - zero functionality so far.

---
+++ CodeWarsCSharp/CodeWarsCSharp/Kyu3/PaintfuckInterpreter.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeWarsCSharp.Kyu3
+{
+    public class PaintfuckInterpreter
+    {
+        /// <summary>
+        /// https://www.codewars.com/kata/esolang-interpreters-number-3-custom-paintf-star-star-k-interpreter/train/csharp
+        /// </summary>
+        /// <param name="code"></param>
+        /// <param name="iterations"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public static string Interpret(string code, int iterations, int width, int height)
+        {
+            return "";
+        }
+    }
+}
