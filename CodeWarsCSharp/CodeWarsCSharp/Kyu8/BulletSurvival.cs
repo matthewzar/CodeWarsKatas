f538fc013e488ed17e7157b27b0e2ae644d266fa
Unknown <funckymat@gmail.com>
2018-11-29 12:41:43 -0500

    Added Bullet Survival Kyu 8 Kata

---
+++ CodeWarsCSharp/CodeWarsCSharp/Kyu8/BulletSurvival.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsCSharp.Kyu8
+{
+    //Kata: https://www.codewars.com/kata/is-he-gonna-survive/train/csharp
+    public class BulletSurvival
+    {
+        public static bool Hero(int bullets, int dragons)
+        {
+            return bullets / (double)dragons >= 2;
+        }
+    }
+}
---
+++ CodeWarsCSharp/CodeWarsCSharpTests/Kyu8/BulletSurvivalTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+using CodeWarsCSharp.Kyu8;
+
+namespace CodeWarsCSharpTests.Kyu8
+{
+    [TestFixture]
+    public class BulletSurvivalTests
+    {
+        [TestCase(10, 5)]
+        [TestCase(100, 40)]
+        public void ATrueHero(int bullets, int dragons)
+        {
+            Assert.IsTrue(BulletSurvival.Hero(bullets, dragons));
+        }
+
+        [TestCase(4, 5)]
+        [TestCase(1500, 751)]
+        [TestCase(0, 1)]
+        [TestCase(7, 4)]
+        public void AFalseHero(int bullets, int dragons)
+        {
+            Assert.IsFalse(BulletSurvival.Hero(bullets, dragons));
+        }
+    }
+}
