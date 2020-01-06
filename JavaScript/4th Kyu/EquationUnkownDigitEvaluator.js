268bc98ba34a94be0c6a8fbba310c918c132d062
Matthew F <funckymat@gmail.com>
2020-01-06 11:25:57 -0500

    Fixed bug where digit duplication was permitted

--- JavaScript/4th Kyu/EquationUnkownDigitEvaluator.js
+++ JavaScript/4th Kyu/EquationUnkownDigitEvaluator.js
@@ -3,17 +3,18 @@
 //such that the equation is correct, and no leading-zeroes are permitted.
 
 //solveExpression('?*11=??')
-// => 1
-//1*11 === 11
+// => 2
+//2*11 === 22
 //0 is not returned as ?? would become 00, which is not a valid number.
-//2 is not returned (despite being viable, as 1 is smaller)
+//1 is not returned (despite being smaller, as 1 is already present)
 
-function solveExpression(exp) {   
-  //Catering to broken test cases as of 2020-01-06
-  if(exp == "?*11=??" || exp == "??*1=??") return 2;
-
+function solveExpression(exp) { 
   for(var i = 0; i < 10; i++)
   {    
+      //No rune is present twice.
+      if(exp.includes(`${i}`)) continue;
+      
+      //No leading zeroes
       if(i == 0 && exp.match(/[=*+-]\?[\d\?]/)) continue;
       
       var subbed = exp.split("?").join(i).replace("--", "- -").replace("=", "===")
@@ -22,9 +23,3 @@ function solveExpression(exp) {
   
   return -1;
 }
-
-// As the test-cases were broken when publishing this, I don't trust any other 
-// solutions to be listed here as comparison points. 
-// My solution had to be run against the random tests several times before they all
-// passed. ~0.25% failed, despite all 'failed' tests actually being solved with a 
-// smaller number by this solution compared to the expected result)
